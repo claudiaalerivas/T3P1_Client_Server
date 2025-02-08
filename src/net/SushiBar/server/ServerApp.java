6976d9f342c5eab5a3a9893aaae9db29df881eb7
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2025-02-08 21:37:09 +0000

    feat(ServerApp): Se añadio un bucle para recibir la coneccion de diversos clientes implementando la clase de hilo ServeClient

--- src/net/SushiBar/server/ServerApp.java
+++ src/net/SushiBar/server/ServerApp.java
@@ -1,6 +1,9 @@
 package net.SushiBar.server;
 
 import java.net.ServerSocket;
+import java.net.Socket;
+
+import net.SushiBar.server.threads.ServeClient;
 import net.SushiBar.utils.Constants;
 
 public class ServerApp {
@@ -8,5 +11,12 @@ public class ServerApp {
     ServerSocket serverSocket = new ServerSocket(Constants.SERVER_PORT);
     System.out.println("       Sushi Bar      ");
     System.out.println("******   OPEN   ******");
+
+    while (true) {
+      Socket clientSocket = serverSocket.accept();
+      System.out.println("Ha entrado un cliente al restaurante");
+      ServeClient clientHandler = new ServeClient(clientSocket);
+      clientHandler.start();
+    }
   }
 }
